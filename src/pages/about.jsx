463b9ac43d4bc1e82a7ac47771dff8257ee8dffd
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-04 17:23:06 -0300

    Create pages as examples

---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "@/components/navbar";
+
+export default function About() {
+  return (
+    <section>
+      <Navbar />
+      <h2>About me</h2>
+    </section>
+  );
+}
---
+++ src/pages/contact.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "@/components/navbar";
+
+export default function Contact() {
+  return (
+    <section>
+      <Navbar />
+      <h2>Contact me</h2>
+    </section>
+  );
+}
