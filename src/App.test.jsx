0ca1d7bd4d7244cfb10239a63005c7f7a970b242
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-08 23:02:05 -0300

    Add new test and fix the another one

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,14 +4,36 @@
 
 import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import fetchMock from "jest-fetch-mock";
+
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import MainApp from "./App";
 
-test("About page renders correctly", async () => {
-  render(
-    <MemoryRouter initialEntries={["/"]}>
-      <About />
-    </MemoryRouter>
-  );
+fetchMock.enableMocks();
 
-  await waitFor(() => expect(screen.getByText(/About me/i)));
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+describe("The app", () => {
+  test("renders the about page correctly", async () => {
+    render(
+      <MemoryRouter initialEntries={["/about"]}>
+        <About />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => expect(screen.getByText(/About me/i)));
+  });
+
+  test("renders the contact page correctly", async () => {
+    render(
+      <MemoryRouter initialEntries={["/contact"]}>
+        <Contact />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => expect(screen.getByText(/Contact me/i)));
+  });
 });
