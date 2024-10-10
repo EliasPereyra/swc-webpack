283067982c379d3616c31225690d952c52c0d653
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-09 21:20:51 -0300

    Create a new route for posts

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import MainApp from "./App";
+import Posts from "./pages/posts";
+import ErrorPage from "./pages/error-page";
 
 export const MainRouter = () => (
   <BrowserRouter>
@@ -10,6 +12,8 @@ export const MainRouter = () => (
       <Route path="/" element={<MainApp />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   </BrowserRouter>
 );
