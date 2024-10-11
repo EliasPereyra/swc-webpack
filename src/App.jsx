a64003c0d00f70c10f99ce996816f7fbf3ea793c
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-11 17:51:59 -0300

    Extract the logic code to the posts page

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { useEffect, useState } from "react";
-import { Navbar } from "./components/navbar.jsx";
+import { Navbar } from "@/components/navbar.jsx";
 
 import "./App.css";
 
-export function MainApp() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    async function getPostData() {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await res.json();
-
-      setPosts(data);
-    }
-
-    getPostData();
-  }, []);
-
+export default function MainApp() {
   return (
     <>
       <Navbar />
-      <main className="mainApp-container">
+      <main className="main-container">
         <h1>Webpack with SWC</h1>
         <p>This is the main app</p>
-
-        <section className="posts-container">
-          <h2>Posts</h2>
-          <ul>
-            {posts.map((post) => (
-              <li key={post.id}>{post.title}</li>
-            ))}
-          </ul>
-        </section>
       </main>
     </>
   );
