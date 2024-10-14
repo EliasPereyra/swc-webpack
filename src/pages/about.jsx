f95d3fd8cacfb598a2fd0c1e2a98255ae513f6e7
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-10-13 23:27:26 -0300

    Create an about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,18 @@
 import { Navbar } from "@/components/navbar";
 
+import "./about.css";
+
 export default function About() {
   return (
-    <section>
+    <>
       <Navbar />
-      <h2>About me</h2>
-    </section>
+      <section className="about-container">
+        <h2>About me</h2>
+        <p>
+          Elias Pereyra, software developer based in Argentina, with a lot of
+          curiosity.
+        </p>
+      </section>
+    </>
   );
 }
