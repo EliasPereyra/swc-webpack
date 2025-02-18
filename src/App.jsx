a2a5404439144535d7f90a3438649a72a42b12ef
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-02-18 01:11:21 -0300

    Add three cards to the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import { Navbar } from "@/components/navbar.jsx";
+import { Card } from "@/components/card";
 
 import "./App.css";
+
+const cards = [
+  {
+    title: "Webpack, an extensible bundler",
+    description:
+      "You can add any kind of plugin you want to get the most out of Webpack",
+  },
+  {
+    title: "SWC, a compiler",
+    description:
+      "A wonderful compiler written in Rust that can transpile JavaScript very fast",
+  },
+  {
+    title: "Webpack with Rust",
+    description: "A good combination which makes Webpack to work even faster",
+  },
+];
 
 export default function MainApp() {
   return (
@@ -8,7 +26,22 @@ export default function MainApp() {
       <Navbar />
       <main className="main-container">
         <h1>Webpack with SWC</h1>
-        <p>This is the main app</p>
+        <p className="main-description">
+          Learn how to build a React SPA with <strong>Webpack</strong> and{" "}
+          <strong>SWC</strong> from scratch and earn a better understanding of
+          one of the most popular JavaScript build tools
+        </p>
+        <a href="https://github.com/EliasPereyra/swc-webpack">Get started</a>
+
+        <ul className="card-container">
+          {cards.map((card) => (
+            <Card
+              key={card.title}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
+        </ul>
       </main>
     </>
   );
