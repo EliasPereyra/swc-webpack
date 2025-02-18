064019b0f516725b2a90ed5647dcebd7769809a3
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-02-18 01:12:16 -0300

    Add a small description

--- src/pages/posts.jsx
+++ src/pages/posts.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Navbar } from "../components/navbar";
 
+import "./posts.css";
+
 export default function Posts() {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -28,6 +30,16 @@ export default function Posts() {
 
       <section className="posts-container">
         <h2>Posts</h2>
+        <p className="posts-description">
+          Using the{" "}
+          <a
+            href="https://jsonplaceholder.typicode.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            JSONPlaceholder API
+          </a>
+        </p>
         <ul>
           {posts.length > 0 ? (
             posts.map((post) => <li key={post.id}>{post.title}</li>)
